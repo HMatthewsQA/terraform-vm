0e4005573aa24d966bdc6b30546ce9a958fff624
HMatthewsQA <HMatthews@academytrainee.com>
2020-07-27 14:25:40 +0100

    remove replica code

--- main.tf
+++ main.tf
@@ -10,7 +10,3 @@ module "azurevirtualmachine" {
   source = "./VirtualMachine"
 }
 
-data "azurerm_public_ip" "example" {
-  name                = azurerm_public_ip.example.name
-  resource_group_name = azurerm_linux_virtual_machine.example.resource_group_name
-}
