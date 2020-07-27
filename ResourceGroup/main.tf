8ab1d4ad6e6d14355ab98f68b66e76fe9196369b
HMatthewsQA <HMatthews@academytrainee.com>
2020-07-27 14:10:12 +0100

    rename

--- ResourceGroup/main.tf
+++ ResourceGroup/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-data "resource_group_name" "example" {
+data "resource_group" "example" {
   name                = azurerm_resource_group.example.name
   resource_group_name = azurerm_resource_group.example.resource_group_name
 }
