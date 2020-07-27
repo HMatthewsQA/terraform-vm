4fe4c84767fef42dd6d39eb443e7dd1d057e4235
HMatthewsQA <HMatthews@academytrainee.com>
2020-07-27 12:27:56 +0100

    typo

--- ResourceGroup/main.tf
+++ ResourceGroup/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-data "resouce_group_name" "example" {
+data "resource_group_name" "example" {
   name                = azurerm_resource_group.example.name
   resource_group_name = azurerm_resource_group.example.resource_group_name
 }
